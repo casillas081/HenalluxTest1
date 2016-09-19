5c666c9a66b5a59dd89387e9d733e932bae6e27c
casillas081 <christ.schepmans@hotmail.com>
2016-09-19 11:34:15 +0200

    Premier envoi labo1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pupil eleve = new Pupil("Schepmans", 22);
+            Activity activities = new Activity("Principe de programmation", true);
+            Activity activities1 = new Activity("Néerlandais", false);
+            Activity activities2 = new Activity("Mathématique", true);
+
+            eleve.AddActivity(activities);
+            eleve.AddActivity(activities1);
+            eleve.AddActivity(activities2);
+
+            eleve.AddEvalutation("Principe de programmation");
+            eleve.AddEvalutation(evaluation: 'T', title: "Mathématique");
+            eleve.AddEvalutation("Néerlandais");
+
+            List<Pupil> lstPupils = new List<Pupil>()
+            {
+                new Pupil("Guebel", 10, 5),
+                new Pupil("Rouffi", 7),
+                new Pupil("Michou", 10),
+                new Pupil("Delcourt", 8, 3),
+                new Pupil("Herou", 4),
+            };
+
+            var pupilGrade1Plus6 = from 
+
+            System.Console.Write(eleve);
+            System.Console.Read();
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Pupil.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Pupil : Person
+    {
+        public int Grade { get; set; }
+
+        private List<Activity> lstActivities;
+
+        public List<Activity> LstActivities
+        {
+            get { return lstActivities; }
+            set { lstActivities = value; }
+        }
+
+
+
+        private char[] tabEval;
+
+        public char[] TabEval
+        {
+            get { return tabEval; }
+            set { tabEval = value; }
+        }
+
+
+        public Pupil(string name, int age, int grade) : base(name, age)
+        {
+            Grade = grade;
+            LstActivities = new List<Activity>();
+            TabEval = new char[10];
+        }
+
+        public Pupil(String name, int age) : this(name, age, 1) { }
+
+        public void AddActivity(Activity activity)
+        {
+            LstActivities.Add(activity);
+        }
+
+        public void AddEvalutation(String title = null, char evaluation = 'S')
+        {
+            for(int i = 0; i < TabEval.Length && i < LstActivities.Count(); i++)
+            {
+                if (LstActivities.ElementAt(i).Equals(title))
+                {
+                    TabEval[i] = evaluation;
+                }
+            }
+        }
+
+        public override String ToString()
+        {
+            string ch = base.ToString(); // reprend le toString de Person
+            int cptActivities = LstActivities.Count(); // mets le nbr d'activité
+            if(cptActivities == 0)
+            {
+                ch += " n'a pas encore choisi d'activité";
+            }
+            else
+            {
+                ch += " a comme activitée(s) :";
+                foreach (Activity activity in LstActivities)
+                {
+                    ch += " "+activity + " ";
+                }
+            }
+
+            return ch;
+        }
+    }
+}
